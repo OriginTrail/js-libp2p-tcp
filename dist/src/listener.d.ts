262c89769651c8c087281a0451aad8569096598e
zeroxbt <original.swazi@protonmail.com>
2022-09-26 13:42:27 +0200

    add dist folder

---
+++ dist/src/listener.d.ts
@@ -0,0 +1,16 @@
+import type { Connection } from '@libp2p/interface-connection';
+import type { Upgrader, Listener } from '@libp2p/interface-transport';
+import type { TCPCreateListenerOptions } from './index.js';
+interface Context extends TCPCreateListenerOptions {
+    handler?: (conn: Connection) => void;
+    upgrader: Upgrader;
+    socketInactivityTimeout?: number;
+    socketCloseTimeout?: number;
+    publicIp?: string;
+}
+/**
+ * Create listener
+ */
+export declare function createListener(context: Context): Listener;
+export {};
+//# sourceMappingURL=listener.d.ts.map
